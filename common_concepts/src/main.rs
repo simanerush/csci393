48552e5a72f452f053b84cab419e95b44b0c2e6a
Sima Nerush <2002ssn@gmail.com>
2023-01-31 20:51:50 -0800

    add common concepts

--- common_concepts/src/main.rs
+++ common_concepts/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
+use std::io;
 
+fn main() {
+    // constant declaration
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // shadowing
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    let a = [1, 2, 3, 4, 5];
+    println!("please enter an array index");
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("index entered was not a number");
+
+    let element = a[index];
+    println!("the value of the element at index {index} is: {element}");
 }
